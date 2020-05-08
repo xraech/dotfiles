124fdf68b73829a23de0a3b56cd33d692ec05c0e
xraech <rrg.grossi@gmail.com>
2020-05-08 18:36:48 -0300

    fixed file structure

--- gentoo/bin/bspwm/movelayer.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-if bspc query -N -n 'focused.!normal' > /dev/null; then
-        bspc node -l normal
-else
-        bspc node -l above
-fi
--- gentoo/bin/bspwm/rules/external_rules.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/dash
-wmtitle=$(xprop -id "$1"  WM_NAME | cut -d\" -f2 | cut -d\| -f1)
-if [ "$wmtitle" = "yuzu" ];
-then
-    echo state=fullscreen desktop'^4' follow=on
-fi
